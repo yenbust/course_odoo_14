d11e084ab9db5514728508c55aa413643a847e1a
Yenbust <yenbust@gmail.com>
2021-11-02 15:05:54 +0000

    App Odoo Academy

---
+++ odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,34 @@
+# _*_ coding: utf-8 -*-
+
+{
+
+   'name': 'Odoo Academy',
+
+   'summary': 'Aplicar descuentos en la interfaz del POS',
+
+   'version': '12.0.1.0.0',  
+
+   'description': """ Academy app.a
+        Academy modeule:
+        - Courses
+        - Sessions
+        - Attendees
+    """, 
+
+    
+    'author': 'Yenbust',
+
+  
+    'category': 'Traning',
+    
+    'version': '0.1',
+    
+    'depends': ['base'],
+
+    'data': [],
+    
+    'demo': [],    
+   
+
+
+}
