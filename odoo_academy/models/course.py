2be2cb9f51517375f56a50b5c1e82ac0ff7309f2
Yenbust <yenbust@gmail.com>
2021-11-02 18:41:52 +0000

    Fields of model.

--- odoo_academy/models/course.py
+++ odoo_academy/models/course.py
@@ -5,3 +5,13 @@ from odoo import models, fields, api
 class Course(models.Model):
     _name = 'academy.course'
     _description = 'Course info'
+    
+    name = fields.Char(string='Title', required=True)
+    description = fields.Text(string='Description')
+    level = fields.Selection(string='Level',
+                            selection=[('beginner', 'Beginner'),
+                                      ('intermediate', 'Intermediate'),
+                                      ('advanced', 'Advanced')],
+                            copy=False)
+    active = fields.Boolean(string='Active', default=True)
+    
